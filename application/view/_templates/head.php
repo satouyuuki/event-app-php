0fc44b2efafc6a8a415e80a33675d95641ec85e7
satoyuki <yuki01377@gmail.com>
2020-05-24 19:27:54 +0900

    google analytics 導入

--- application/view/_templates/head.php
+++ application/view/_templates/head.php
@@ -2,6 +2,16 @@
 <html lang="ja">
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167510796-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-167510796-1');
+    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="このwebサービスはご自身のイベントを管理するメモ帳です。
